33c2808883074bbc145cd03710c04176c9efb104
sebastian martinez <sms061987@sebastian-martinezs-MacBook-Pro.local>
2016-09-01 12:10:07 -0700

    grid is completed with new css stylying and DnD functionality. Images are stored in Json object

--- grid.js
+++ grid.js
@@ -2,6 +2,56 @@
  * Created by sms061987 on 8/30/16.
  */
 
+
+// Json object holding images
+var data = {
+    "images": [
+        {"bannerImg": "images/1.png"},
+        {"bannerImg": "images/2.png"},
+        {"bannerImg": "images/3.png"},
+        {"bannerImg": "images/4.png"},
+        {"bannerImg": "images/5.png"},
+        {"bannerImg": "images/6.png"},
+        {"bannerImg": "images/7.png"},
+        {"bannerImg": "images/8.png"},
+        {"bannerImg": "images/9.png"},
+        {"bannerImg": "images/10.png"},
+        {"bannerImg": "images/11.png"},
+        {"bannerImg": "images/12.png"},
+    ]
+};
+
+// Once the document is finished loading call the createGrid() function
+window.onload = createGrid();
+
+// This function will create the grid of images and divs needed
+function createGrid() {
+
+    // get reference of container waiting in HTML file
+    var mainContainer = document.getElementById("main-container");
+
+    data.images.forEach(function (obj) {
+
+        // create div element which will hold each image
+        var iDiv = document.createElement('div');
+        iDiv.className = 'inline-container';
+
+        // create image that goes inside <div> element
+        var img = document.createElement('img');
+        img.src = obj.bannerImg;
+
+        // insert image into div
+        iDiv.appendChild(img);
+
+        // insert div into main
+        mainContainer.appendChild(iDiv);
+    });
+
+}
+
+
+// Below are the function for DnD
+
 var dragSrcEl = null;
 
 function allowDrop(ev) {
@@ -19,7 +69,7 @@ function drop(e) {
 }
 
 var cols = document.querySelectorAll('#main-container .inline-container');
-[].forEach.call(cols, function(col) {
+[].forEach.call(cols, function (col) {
     col.addEventListener('dragstart', drag, false);
     col.addEventListener('dragover', allowDrop, false);
     col.addEventListener('drop', drop, false);
