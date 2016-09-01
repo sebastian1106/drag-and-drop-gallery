490926af0df93024355755f70e54c9f92248ee53
sebastian martinez <sms061987@sebastian-martinezs-MacBook-Pro.local>
2016-08-31 23:27:49 -0700

    images are now movable

--- grid.js
+++ grid.js
@@ -4,6 +4,10 @@
 
 var dragSrcEl = null;
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
 function drag(e) {
     dragSrcEl = this;
     e.dataTransfer.setData('text/html', this.innerHTML);
@@ -13,3 +17,10 @@ function drop(e) {
     dragSrcEl.innerHTML = this.innerHTML;
     this.innerHTML = e.dataTransfer.getData('text/html');
 }
+
+var cols = document.querySelectorAll('#main-container .inline-container');
+[].forEach.call(cols, function(col) {
+    col.addEventListener('dragstart', drag, false);
+    col.addEventListener('dragover', allowDrop, false);
+    col.addEventListener('drop', drop, false);
+});
